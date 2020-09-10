d808e84b10c95dbc54ccded4f5604f47c3a55fa0
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-09-10 13:32:27 -0700

    Add home page and global reset styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,8 @@
 <script>
-
 </script>
 
 <style>
-  h1,
+  /* h1,
   p {
     text-align: center;
     margin: 0 auto;
@@ -24,16 +23,19 @@
     h1 {
       font-size: 4em;
     }
-  }
+  } */
 </style>
 
 <svelte:head>
-  <title>TODO: Title</title>
+  <title>Svelte Front End Mentor Challenges</title>
 </svelte:head>
 
 <h1>Great success!</h1>
 
-<p>
-  <strong>Try editing this file (src/routes/index.svelte) to test live
-    reloading.</strong>
-</p>
+<section class="container">
+  <article>
+    <h3><a href="article-preview-component">Article preview component</a></h3>
+    <p>Started 9/10/2020</p>
+    <p>time: ...</p>
+  </article>
+</section>
