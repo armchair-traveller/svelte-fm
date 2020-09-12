adb40a8273656de97b0b923351b8c4a12c658665
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-09-12 07:01:29 -0700

    update main page to not look bad :P

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,40 +2,24 @@
 </script>
 
 <style>
-  /* h1,
-  p {
-    text-align: center;
-    margin: 0 auto;
+  .container {
+    font-family: Arial, Helvetica, sans-serif;
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
   }
-
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
-    }
-  } */
 </style>
 
 <svelte:head>
   <title>Svelte Front End Mentor Challenges</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
 <section class="container">
+  <h1>Great success!</h1>
+
   <article>
     <h3><a href="article-preview-component">Article preview component</a></h3>
     <p>Started 9/10/2020</p>
-    <p>time: ...</p>
+    <p>time: 6 hours</p>
   </article>
 </section>
