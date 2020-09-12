5168390c91001bb16c2c5d8bfdd14063905eb395
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-09-12 05:56:48 -0700

    Finalize article-preview-component

--- src/routes/article-preview-component.svelte
+++ src/routes/article-preview-component.svelte
@@ -1,3 +1,10 @@
+<script>
+  import { fade } from "svelte/transition";
+  let active = false;
+  let activeReady = true;
+  let innerWidth;
+</script>
+
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Manrope:wght@500;700&display=swap");
   :root {
@@ -5,6 +12,7 @@
     --blue-200: hsl(212, 23%, 69%);
     --blue-300: hsl(214, 17%, 51%);
     --blue-400: hsl(217, 19%, 35%);
+    background: var(--blue-100);
   }
   * {
     font-family: "Manrope", sans-serif;
@@ -21,8 +29,8 @@
 
   .container {
     margin: 0 auto;
-    background: var(--blue-100);
     min-height: 100vh;
+    min-width: 100%;
     display: flex;
     flex-flow: column;
     align-items: center;
@@ -42,7 +50,6 @@
     align-items: center;
     max-width: 330px;
     background-color: white;
-    overflow: hidden;
     border-radius: 0.6rem;
   }
 
@@ -50,11 +57,12 @@
     width: 330px;
     height: 200px;
     object-fit: cover;
+    border-radius: 0.6rem 0.6rem 0 0;
   }
 
   .content {
     padding: 2rem 2rem;
-    padding-bottom: 1.2rem;
+    padding-bottom: 0;
   }
 
   h2 {
@@ -78,6 +86,10 @@
     align-content: center;
     justify-content: space-between;
     align-items: center;
+    height: 5rem;
+    width: 100%;
+    padding: 0 2rem;
+    border-radius: 0 0 0.6rem 0.6rem;
   }
   .bottom img {
     border-radius: 50%;
@@ -104,15 +116,17 @@
     background-color: var(--blue-100);
     border: none;
     border-radius: 50%;
-    width: 30px;
-    height: 30px;
-    overflow: none;
+    width: 2rem;
+    height: 2rem;
+    overflow: hidden;
     display: flex;
     align-items: center;
+    justify-content: center;
   }
   .share img {
-    height: 15px;
+    height: 13px;
     margin: 0;
+    border-radius: 0;
   }
 
   .date {
@@ -126,7 +140,101 @@
     justify-content: center;
   }
 
-  @media (min-width: 768px) {
+  .bottom.active {
+    background-color: var(--blue-400);
+  }
+
+  .share.active {
+    background-color: var(--blue-300);
+  }
+
+  button.share.active > img {
+    filter: brightness(0) invert(1);
+  }
+
+  div.share-btns > p {
+    margin: 0;
+    font-size: 0.8rem;
+    letter-spacing: 0.3rem;
+  }
+
+  div.share-btns > button {
+    height: 20px;
+    background: none;
+    border: none;
+    padding: 0;
+    margin: 0 0.5rem;
+  }
+
+  div.share-btns > button:first-of-type {
+    margin-left: 1rem;
+  }
+
+  div.share-btns img {
+    width: 20px;
+    height: 20px;
+    margin: 0;
+    padding: 0;
+    border-radius: 0;
+  }
+
+  /* desktop dimensions */
+  @media (min-width: 850px) {
+    article {
+      max-width: 770px;
+      display: grid;
+      grid-template-columns: 20rem 1fr;
+      grid-template-areas:
+        "image details"
+        "image bottom";
+    }
+    .drawers {
+      grid-area: image;
+      height: 20rem;
+      width: 20rem;
+      border-radius: 0.6rem 0 0 0.6rem;
+    }
+    .content {
+      grid-area: details;
+      align-self: end;
+    }
+    .bottom {
+      grid-area: bottom;
+      align-self: start;
+    }
+
+    div.content h2 {
+      font-size: 1.3rem;
+    }
+
+    .d-share {
+      position: relative;
+    }
+    .d-share div.share-btns {
+      position: absolute;
+      background-color: var(--blue-400);
+      width: 15rem;
+      height: 3rem;
+      display: flex;
+      flex-flow: row nowrap;
+      align-items: center;
+      justify-content: center;
+      border-radius: 0.6rem;
+      left: -6.5rem;
+      top: -4.75rem;
+      box-shadow: 0 0.5rem 2px 2px rgba(178, 178, 178, 0.1);
+    }
+    .share-btns::before {
+      background-color: var(--blue-400);
+      content: "";
+      display: block;
+      height: 16px;
+      width: 16px;
+      position: absolute;
+      bottom: -7.5px;
+      transform: rotate(47deg) skew(5deg);
+      box-shadow: 0 2px 2px 0 rgba(178, 178, 178, 0.4);
+    }
   }
 </style>
 
@@ -143,6 +251,8 @@
 
   <title>Frontend Mentor | Article preview component</title>
 </svelte:head>
+
+<svelte:window bind:innerWidth />
 
 <section class="container">
   <article>
@@ -161,7 +271,9 @@
         slightly bare and uninviting. I’ve got some simple tips to help you make
         any room feel complete.
       </p>
-
+    </div>
+    {#if innerWidth >= 850}
+      <!-- desktop interaction -->
       <div class="bottom">
         <div>
           <img
@@ -173,15 +285,77 @@
           </div>
         </div>
 
-        <button class="share">
-          <img src="./article-preview-component/icon-share.svg" alt="Share" /></button>
+        <div class="d-share">
+          <button
+            class="share"
+            class:active
+            on:click={() => (active = !active)}>
+            <img src="./article-preview-component/icon-share.svg" alt="Share" />
+          </button>
+
+          {#if active}
+            <!-- popup share -->
+            <div class="share-btns" transition:fade>
+              <p>SHARE</p>
+              <button><img
+                  src="./article-preview-component/icon-facebook.svg"
+                  alt="Facebook" /></button>
+              <button><img
+                  src="./article-preview-component/icon-twitter.svg"
+                  alt="Twitter" /></button>
+              <button><img
+                  src="./article-preview-component/icon-pinterest.svg"
+                  alt="Pinterest" /></button>
+            </div>
+          {/if}
+        </div>
       </div>
-    </div>
+    {:else if !active && activeReady}
+      <div class="bottom" in:fade>
+        <div>
+          <img
+            src="./article-preview-component/avatar-michelle.jpg"
+            alt="Michelle's Avatar" />
+          <div>
+            <h3>Michelle Appleton</h3>
+            <p class="date">28 Jun 2020</p>
+          </div>
+        </div>
+
+        <button class="share" on:click={() => (active = !active)}>
+          <img src="./article-preview-component/icon-share.svg" alt="Share" />
+        </button>
+      </div>
+    {:else if activeReady}
+      <div
+        class="bottom active"
+        transition:fade
+        on:outrostart={() => (activeReady = false)}
+        on:outroend={() => (activeReady = true)}>
+        <div class="share-btns">
+          <p>SHARE</p>
+          <button><img
+              src="./article-preview-component/icon-facebook.svg"
+              alt="Facebook" /></button>
+          <button><img
+              src="./article-preview-component/icon-twitter.svg"
+              alt="Twitter" /></button>
+          <button><img
+              src="./article-preview-component/icon-pinterest.svg"
+              alt="Pinterest" /></button>
+        </div>
+        <button class="share active" on:click={() => (active = !active)}>
+          <img src="./article-preview-component/icon-share.svg" alt="Share" />
+        </button>
+      </div>
+    {/if}
   </article>
 
   <div class="attribution" style="margin-top:1rem;">
     Challenge by <a
       href="https://www.frontendmentor.io?ref=challenge"
-      target="_blank">Frontend Mentor</a>. Coded by <a href="_">Fang🦁</a>.
+      target="_blank">Frontend Mentor</a>. Coded by <a
+      href="https://github.com/armchair-traveller"
+      target="_blank">Fang🦁</a>.
   </div>
 </section>
