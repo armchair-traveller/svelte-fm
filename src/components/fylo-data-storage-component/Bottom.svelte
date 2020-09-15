febedcb2461312ad0701aabcdc1d413621693a00
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-09-14 21:27:56 -0700

    format markup

--- src/components/fylo-data-storage-component/Bottom.svelte
+++ src/components/fylo-data-storage-component/Bottom.svelte
@@ -1,10 +1,6 @@
-
-
 <div class="box">
-  You’ve used 815 GB of your storage
-
-  185 GB Left
-  
-  0 GB
-  1000 GB
+  <p>You’ve used 815 GB of your storage</p>
+  <p>0 GB</p>
+  <p>1000 GB</p>
+  <h2>185 GB Left</h2>
 </div>
