f5a008938482c354bff761162172a83ac4851d4a
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-09-18 21:13:58 -0700

    add a few assets

--- src/components/fylo-data-storage-component/Top.svelte
+++ src/components/fylo-data-storage-component/Top.svelte
@@ -1,4 +1,8 @@
+<script>
+  const assets = "./fylo-data-stored-component";
+</script>
 
 <div class="box">
-  
+  <img src="{assets}/icon-folder.svg" alt="" />
+  <img src="{assets}/icon-upload.svg" alt="" />
 </div>
