a8945ed947e6855a26d946eca714bdd6921d7562
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-09-20 17:10:50 -0700

    Add logo

--- src/components/fylo-data-storage-component/Top.svelte
+++ src/components/fylo-data-storage-component/Top.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <div class="box">
+  <img src="{assets}/logo.svg" alt="Fylo Logo" />
   <img src="{assets}/icon-document.svg" alt="Document" />
   <img src="{assets}/icon-folder.svg" alt="Folder" />
   <img src="{assets}/icon-upload.svg" alt="Upload" />
