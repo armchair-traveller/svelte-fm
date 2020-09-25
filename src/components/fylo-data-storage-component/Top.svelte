c24fb9cb55d5ac1fe8ccdb344676a718005d2031
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-09-25 02:41:57 -0700

    structure markup for flex

--- src/components/fylo-data-storage-component/Top.svelte
+++ src/components/fylo-data-storage-component/Top.svelte
@@ -2,9 +2,19 @@
   const assets = "./fylo-data-storage-component";
 </script>
 
+<style>
+  .box {
+    display: flex;
+    flex-flow: column;
+    align-items: flex-start;
+  }
+</style>
+
 <div class="box">
   <img src="{assets}/logo.svg" alt="Fylo Logo" />
-  <img src="{assets}/icon-document.svg" alt="Document" />
-  <img src="{assets}/icon-folder.svg" alt="Folder" />
-  <img src="{assets}/icon-upload.svg" alt="Upload" />
+  <div>
+    <img src="{assets}/icon-document.svg" alt="Document" />
+    <img src="{assets}/icon-folder.svg" alt="Folder" />
+    <img src="{assets}/icon-upload.svg" alt="Upload" />
+  </div>
 </div>
