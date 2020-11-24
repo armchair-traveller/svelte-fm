d609c4b06a01fc5128d237d1d807d2366702a886
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-11-24 01:00:36 -0800

    add bold, palceholder for progress

--- src/components/fylo-data-storage-component/Bottom.svelte
+++ src/components/fylo-data-storage-component/Bottom.svelte
@@ -4,16 +4,18 @@
     width: 90%;
     margin: 0 auto;
     border-radius: 0.5rem;
-    padding: 2rem;
+    padding: 1.5rem 2rem;
     padding-bottom: 0;
   }
-  p {
-    color: var(--blue-100);
+
+  progress[value]::-webkit-progress-bar {
+    background-color: blue;
   }
 </style>
 
 <div class="box">
-  <p>You’ve used 815 GB of your storage</p>
+  <p>You’ve used <strong>815 GB</strong> of your storage</p>
+  <div class="bar"><progress value="0.815" /></div>
   <p>0 GB</p>
   <p>1000 GB</p>
   <h2>185 GB Left</h2>
