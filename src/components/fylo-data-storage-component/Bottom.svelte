2d545c19284287c1d66f064ca35c2b77d1083526
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-11-25 03:35:42 -0800

    delete progress bar, can't be styled

--- src/components/fylo-data-storage-component/Bottom.svelte
+++ src/components/fylo-data-storage-component/Bottom.svelte
@@ -8,14 +8,18 @@
     padding-bottom: 0;
   }
 
-  progress[value]::-webkit-progress-bar {
-    background-color: blue;
+  .bar {
+    width: 100px;
+    height: 20px;
+    background-image: linear-gradient(hsl(6, 100%, 80%), hsl(335, 100%, 65%));
+    border: 5px solid var(--blue-200);
+    border-radius: 1rem;
   }
 </style>
 
 <div class="box">
   <p>You’ve used <strong>815 GB</strong> of your storage</p>
-  <div class="bar"><progress value="0.815" /></div>
+  <div class="bar" />
   <p>0 GB</p>
   <p>1000 GB</p>
   <h2>185 GB Left</h2>
