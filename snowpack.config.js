91a76dba14770c7e135a1ff41d8a34e59b41e4e5
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-12-04 08:22:35 -0800

    use new aliasing syntax

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,7 +1,18 @@
 // Consult https://www.snowpack.dev to learn about these options
 module.exports = {
-  plugins: [["@snowpack/plugin-build-script", { "cmd": "postcss", "input": [".css", ".pcss"], "output": [".css"] }]],
-	extends: "@sveltejs/snowpack-config",
+  plugins: [
+    [
+      "@snowpack/plugin-build-script",
+      { cmd: "postcss", input: [".css", ".pcss"], output: [".css"] },
+    ],
+  ],
+  extends: "@sveltejs/snowpack-config",
+  mount: {
+    "src/components": "/_components",
+  },
+  alias: {
+    $components: "./src/components",
+  },
   buildOptions: {
     souceMaps: true,
   },
